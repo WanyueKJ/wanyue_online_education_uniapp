37d0a6d05f56e2ed7aa4af953b04b49f26e14aaa
shuyinghengxieshuiqingqian <lj5211zl@163.com>
2021-05-26 18:06:03 +0800

    #task 优化

--- pages/coursemore/coursemore.vue
+++ pages/coursemore/coursemore.vue
@@ -68,7 +68,7 @@
 			
 			viewCourseInfo(contentCourseId, contentCoursetype){
 
-				if (getApp().globalData.userinfo == '') {
+				if (app.globalData.userinfo == '') {
 					uni.navigateTo({
 						url: '../login/login'
 					})
