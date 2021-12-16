c5bbaae38967bcdf0298a54fffba1ea9d3cb82bb
shuyinghengxieshuiqingqian <lj5211zl@163.com>
2021-12-16 17:57:42 +0800

    #task 维护

--- pages/edit_cv/edit_cv.vue
+++ pages/edit_cv/edit_cv.vue
@@ -5,7 +5,7 @@
 		<uni-nav-bar statusBar title="编写简历" :border="false" leftIcon="back" @clickLeft="back" @clickRight="subCv">
 			<view class="sub-btn" @click="subCv" slot="right">提交</view>
 		</uni-nav-bar>
-		
+
 		<!-- 主题内容区域 -->
 		<view>
 			<view class="jianli-item yuanxiao">
@@ -20,19 +20,19 @@
 			</view>
 			<view class="jianli-item">
 				<text>教学特点</text>
-				<textarea class="tedian" v-model="feature" value="" placeholder="请填写你的教学特点或个人风格, 最多200字" 
+				<textarea class="tedian" v-model="feature" value="" placeholder="请填写你的教学特点或个人风格, 最多200字"
 				placeholder-class="place_zhan" />
 			</view>
-			
+
 		</view>
-		
+
 	</view>
 </template>
 
 <script>
 	import uniNavBar from '@/components/uni-ui/uni-nav-bar/uni-nav-bar.vue';
 	const app = getApp();
-	
+
 	export default {
 		components:{
 			uniNavBar
@@ -60,10 +60,10 @@
 						'uid' : gData.userinfo.id,
 						'token' : gData.userinfo.token,
 						'fields' : JSON.stringify({
-										'school' : this.school,
-										'experience' : this.experience,
-										'feature' : this.feature,
-								   }),
+							'school' : this.school,
+							'experience' : this.experience,
+							'feature' : this.feature,
+					   }),
 					},
 					success: res => {
 						uni.showToast({
@@ -90,50 +90,50 @@
 </script>
 
 <style>
-	
+
 	/* 占位符样式 */
 	.place_zhan {
 		color:#C8C8C8;
 		font-size:26rpx;
 		padding-left:20rpx;
 	}
-	
+
 	.cv-all-wrap {
 		width: 90%;
 		margin: 0 auto;
 	}
-	
+
 	.sub-btn {
 		color: #38DAA6;
 	}
-	
+
 	.school {
 		height: 72rpx;
 	}
-	
+
 	.jianli-item {
 		margin-top: 35rpx;
 	}
-	
+
 	.jianli-item > text {
 		display: inline-block;
 		margin-bottom: 10rpx;
 	}
-	
+
 	.jianli-item > input,
 	.jianli-item > textarea {
 		width: 100%;
 		background-color: #FAFAFA;
 	}
-	
+
 	/* 多行文本框 */
 	.jiaoxue {
 		height: 200rpx;
 	}
-	
+
 	.tedian {
 		height: 400rpx;
 	}
-	
-	
+
+
 </style>
